518fb2a25e87820457e610f05943e95a48d287e5
rronsa <ronja.salo9@gmail.com>
2016-04-22 12:22:15 +0300

    Added files via upload

---
+++ script.js
@@ -0,0 +1,268 @@
+/*********************************************************************************
+										!!!! LOGIN.JS BÖRJAR HÄR !!!!
+**********************************************************************************/
+(function($) {
+    "use strict";
+	
+	// Options for Message
+	//----------------------------------------------
+  var options = {
+	  'btn-loading': '<i class="fa fa-spinner fa-pulse"></i>',
+	  'btn-success': '<i class="fa fa-check"></i>',
+	  'btn-error': '<i class="fa fa-remove"></i>',
+	  'msg-success': 'All Good! Redirecting...',
+	  'msg-error': 'Wrong login credentials!',
+	  'useAJAX': true,
+  };
+
+	// Login Form
+	//----------------------------------------------
+	// Validation
+  $("#login-form").validate({
+  	rules: {
+      lg_username: "required",
+  	  lg_password: "required",
+    },
+  	errorClass: "form-invalid"
+  });
+  
+	// Form Submission
+  $("#login-form").submit(function() {
+  	remove_loading($(this));
+		
+		if(options['useAJAX'] == true)
+		{
+			// Dummy AJAX request (Replace this with your AJAX code)
+		  // If you don't want to use AJAX, remove this
+  	  dummy_submit_form($(this));
+		
+		  // Cancel the normal submission.
+		  // If you don't want to use AJAX, remove this
+  	  return false;
+		}
+  });
+	
+	// Register Form
+	//----------------------------------------------
+	// Validation
+  $("#register-form").validate({
+  	rules: {
+      reg_username: "required",
+  	  reg_password: {
+  			required: true,
+  			minlength: 5
+  		},
+   		reg_password_confirm: {
+  			required: true,
+  			minlength: 5,
+  			equalTo: "#register-form [name=reg_password]"
+  		},
+  		reg_email: {
+  	    required: true,
+  			email: true
+  		},
+  		reg_agree: "required",
+    },
+	  errorClass: "form-invalid",
+	  errorPlacement: function( label, element ) {
+	    if( element.attr( "type" ) === "checkbox" || element.attr( "type" ) === "radio" ) {
+    		element.parent().append( label ); // this would append the label after all your checkboxes/labels (so the error-label will be the last element in <div class="controls"> )
+	    }
+			else {
+  	  	label.insertAfter( element ); // standard behaviour
+  	  }
+    }
+  });
+
+  // Form Submission
+  $("#register-form").submit(function() {
+  	remove_loading($(this));
+		
+		if(options['useAJAX'] == true)
+		{
+			// Dummy AJAX request (Replace this with your AJAX code)
+		  // If you don't want to use AJAX, remove this
+  	  dummy_submit_form($(this));
+		
+		  // Cancel the normal submission.
+		  // If you don't want to use AJAX, remove this
+  	  return false;
+		}
+  });
+
+	// Forgot Password Form
+	//----------------------------------------------
+	// Validation
+  $("#forgot-password-form").validate({
+  	rules: {
+      fp_email: "required",
+    },
+  	errorClass: "form-invalid"
+  });
+  
+	// Form Submission
+  $("#forgot-password-form").submit(function() {
+  	remove_loading($(this));
+		
+		if(options['useAJAX'] == true)
+		{
+			// Dummy AJAX request (Replace this with your AJAX code)
+		  // If you don't want to use AJAX, remove this
+  	  dummy_submit_form($(this));
+		
+		  // Cancel the normal submission.
+		  // If you don't want to use AJAX, remove this
+  	  return false;
+		}
+  });
+
+	// Loading
+	//----------------------------------------------
+  function remove_loading($form)
+  {
+  	$form.find('[type=submit]').removeClass('error success');
+  	$form.find('.login-form-main-message').removeClass('show error success').html('');
+  }
+
+  function form_loading($form)
+  {
+    $form.find('[type=submit]').addClass('clicked').html(options['btn-loading']);
+  }
+  
+  function form_success($form)
+  {
+	  $form.find('[type=submit]').addClass('success').html(options['btn-success']);
+	  $form.find('.login-form-main-message').addClass('show success').html(options['msg-success']);
+  }
+
+  function form_failed($form)
+  {
+  	$form.find('[type=submit]').addClass('error').html(options['btn-error']);
+  	$form.find('.login-form-main-message').addClass('show error').html(options['msg-error']);
+  }
+
+	// Dummy Submit Form (Remove this)
+	//----------------------------------------------
+	// This is just a dummy form submission. You should use your AJAX function or remove this function if you are not using AJAX.
+  function dummy_submit_form($form)
+  {
+  	if($form.valid())
+  	{
+  		form_loading($form);
+  		
+  		setTimeout(function() {
+  			form_success($form);
+  		}, 2000);
+  	}
+  }
+	
+})(jQuery);
+
+/*********************************************************************************
+										!!!! UPLOAD.JS BÖRJAR HÄR !!!!
+**********************************************************************************/
+
+(function($) {
+    'use strict';
+
+    // UPLOAD CLASS DEFINITION
+    // ======================
+
+    var dropZone = document.getElementById('drop-zone');
+    var uploadForm = document.getElementById('js-upload-form');
+
+    var startUpload = function(files) {
+        console.log(files)
+    }
+
+    uploadForm.addEventListener('submit', function(e) {
+        var uploadFiles = document.getElementById('js-upload-files').files;
+        e.preventDefault()
+
+        startUpload(uploadFiles)
+    })
+
+    dropZone.ondrop = function(e) {
+        e.preventDefault();
+        this.className = 'upload-drop-zone';
+
+        startUpload(e.dataTransfer.files)
+    }
+
+    dropZone.ondragover = function() {
+        this.className = 'upload-drop-zone drop';
+        return false;
+    }
+
+    dropZone.ondragleave = function() {
+        this.className = 'upload-drop-zone';
+        return false;
+    }
+
+})(jQuery);
+
+/*********************************************************************************
+										!!!! TAG.JS BÖRJAR HÄR !!!!
+**********************************************************************************/
+
+$(document).ready(function () {
+    $('#defaultForm')
+        .find('[name="cities"]')
+    // Revalidate the color when it is changed
+    .change(function (e) {
+        console.warn($('[name="cities"]').val());
+        console.info($('#aa').val());
+        console.info($("#aa").tagsinput('items'));
+        var a = $("#aa").tagsinput('items');
+        console.log(typeof (a));
+        console.log(a.length);
+        $('#defaultForm').bootstrapValidator('revalidateField', 'cities');
+    })
+        .end()
+        .find('[name="cities1"]')
+    // Revalidate the color when it is changed
+    .change(function (e) {
+        console.warn($('[name="cities1"]').val());
+        console.info($('#aa1').val());
+        console.info($("#aa1").tagsinput('items'));
+        var a = $("#aa1").tagsinput('items');
+        console.log(typeof (a));
+        console.log(a.length);
+        $('#defaultForm').bootstrapValidator('revalidateField', 'cities1');
+    })
+        .end()
+     /*   .bootstrapValidator({
+        excluded: ':disabled',
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            cities: {
+                validators: {
+                    notEmpty: {
+                        message: 'Please enter at least one city you like the most'
+                    }
+                }
+            },
+            cities1: {
+                validators: {
+                    callback: {
+                        message: 'Please choose 2-4 color you like most',
+                        callback: function (value, validator) {
+                            // Get the selected options
+                            var options = validator.getFieldElements('cities1').tagsinput('items');
+                            // console.info(options);
+                            return (options !== null && options.length >= 2 && options.length <= 4);
+                        }
+                    }
+                }
+            }
+        }
+    }) */
+        .on('success.form.bv', function (e) {
+        // Prevent form submission
+        e.preventDefault();
+    });
+});
